739916ac3f99a90348f86599f6c26e2bfa756a2c
clairemcollis@gmail.com <clairemcollis@gmail.com>
2024-02-03 21:56:22 +0000

    test output goes to grafana
    multi stage stress test load test that can export to grafana

--- load-test/test-with-stages.js
+++ load-test/test-with-stages.js
@@ -3,14 +3,25 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: '10m', target: 100 },
-    { duration: '30m', target: 100 },
-    { duration: '10m', target: 200 },
-    { duration: '30m', target: 200 },
-    { duration: '10m', target: 300 },
-    { duration: '30m', target: 300 },	  
+    { duration: '20m', target: 10 }, // simulate ramp-up of traffic from 1 to 100 users over 10 minutes.
+    { duration: '30m', target: 10 }, // stay at 100 users for 30 minutes
+    { duration: '10m', target: 50 }, // ramp-up to 200 users over 10 minutes (peak hour starts)
+    { duration: '30m', target: 50 }, // stay at 200 users for 30 minutes
+    { duration: '10s', target: 100 }, //ramp up to 300 users for 10 minutes
+    { duration: '30m', target: 100 },	// stay at 300 users for 30 minutes
+    { duration: "10m", target: 0 }, // ramp-down to 0 users
   ],
+  thresholds: {
+    http_req_failed: [{
+      threshold: 'rate<=0.05',
+      abortOnFail: true,
+    }],
+    http_req_duration: ['p(95)<=100'],
+    checks: ['rate>=0.99'],
+},
 };
+//abourts the test if the error rate goes about 5%
+//abourts the test if the 95 percentile of the request goes above 100ms
 
 export default function () {
   let url = "https://httpbin.test.k6.io/post";
@@ -21,3 +32,4 @@ export default function () {
 
   sleep(Math.random() * 5);
 }
+//k6 cloud test-with-stages.js
